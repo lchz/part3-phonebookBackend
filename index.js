6b88ea4a63725ed2bea2022c42a6def9a9f22d04
chenhuiz <chenhui.zhao@helsinki.fi>
2020-01-25 15:15:36 +0200

    3.3-3.6

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let persons = [
       {
         "name": "Ada Lovelace",
         "number": "39-44-5323523",
-        "id": 4
+        "id": 2
       },
       {
         "name": "Dan Abramov",
@@ -36,6 +36,61 @@ app.get('/info', (req, res) => {
     res.write(new Date().toString())
     res.send()
 })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    console.log('person: ', person)
+    person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name cannot be empty'
+        })
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: 'number cannot be empty'
+        })
+    } 
+    
+    const existence = persons.find(p => p.name === body.name)
+
+    if (existence) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(newPerson)
+    res.json(newPerson)
+})
+
+const generateId = () => {
+    let newId = Math.floor(Math.random() * Math.floor(999999))
+
+    while (persons.find(p => p.id === newId)) {
+        newId = Math.floor(Math.random() * Math.floor(999999))
+    }
+
+    return newId
+}
 
 const PORT = 3001
 app.listen(PORT)
