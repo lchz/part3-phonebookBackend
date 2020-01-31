59947fb31f4ff4c00d2d90e31df3fb66994b99da
chenhuiz <chenhui.zhao@helsinki.fi>
2020-01-31 18:18:51 +0200

    3.8

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const app = express()
 app.use(express.json())
 
 const morgan = require('morgan')
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+
+const newPerson = (req, res, next) => {
+    // console.log('req: ', req)
+    const person = {
+        name: req.body.name,
+        number: req.body.number,
+    }
+    return JSON.stringify(person)
+}
+
+morgan.token('personToken', newPerson)
+
 
 let persons = [
     {
@@ -29,17 +41,17 @@ let persons = [
       },
 ]
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', morgan('tiny'), (req, res) => {
     res.json(persons)
 })
 
-app.get('/info', (req, res) => {
+app.get('/info', morgan('tiny'), (req, res) => {
     res.write(`Phonebook has info for ${persons.length} people\n\n`)
     res.write(new Date().toString())
     res.send()
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', morgan('tiny'), (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(p => p.id === id)
 
@@ -47,13 +59,13 @@ app.get('/api/persons/:id', (req, res) => {
     person ? res.json(person) : res.status(404).end()
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', morgan('tiny'), (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(p => p.id !== id)
     res.status(204).end()
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :personToken'), (req, res) => {
     const body = req.body
 
     if (!body.name) {
@@ -95,4 +107,6 @@ const generateId = () => {
 }
 
 const PORT = 3001
-app.listen(PORT)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
