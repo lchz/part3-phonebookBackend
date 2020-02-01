284e01d14d4f39be818f899f1bc2d853a7ab002a
chenhuiz <chenhui.zhao@helsinki.fi>
2020-02-01 18:03:58 +0200

    frontend prod mode

---
+++ build/precache-manifest.b6b4e75f5fe5de00d0121bc1f2330bc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6309b5de2658acc8491775c8d9c322d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ccb67373ff439054c4c",
+    "url": "/static/js/2.833bcfc5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.833bcfc5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16f2ec884c7e2c495614",
+    "url": "/static/js/main.f5240016.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
