28ec76aef32d35a368e21d6394dd8fed816bcb96
chenhuiz <chenhui.zhao@helsinki.fi>
2020-02-02 20:34:12 +0200

    3.13-3.15

--- index.js
+++ index.js
@@ -3,115 +3,140 @@ const app = express()
 
 app.use(express.json())
 
-const morgan = require('morgan')
-// app.use(morgan('tiny'))
-
 const cors = require('cors')
 app.use(cors())
 
 app.use(express.static('build'))
 
+/** Middleware */
+const morgan = require('morgan')
+// app.use(morgan('tiny'))
+
 const newPerson = (req, res, next) => {
-    // console.log('req: ', req)
     const person = {
         name: req.body.name,
         number: req.body.number
     }
     return JSON.stringify(person)
 }
-
 morgan.token('personToken', newPerson)
 
+/** MongoDB */
+require('dotenv').config()
+const Person = require('./models/person')
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "222-555",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      },
-]
+
+// let persons = [
+//     {
+//         "name": "Arto Hellas",
+//         "number": "222-555",
+//         "id": 1
+//       },
+//       {
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523",
+//         "id": 2
+//       },
+//       {
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345",
+//         "id": 3
+//       },
+//       {
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122",
+//         "id": 4
+//       },
+// ]
 
 app.get('/api/persons', morgan('tiny'), (req, res) => {
-    res.json(persons)
+    Person.find({}).then(people => {
+        res.json(people.map(p => p.toJSON()))
+    })
 })
 
 app.get('/info', morgan('tiny'), (req, res) => {
-    res.write(`Phonebook has info for ${persons.length} people\n\n`)
-    res.write(new Date().toString())
-    res.send()
+    // Person.countDocuments({}).then(result => {
+    //     const response = {
+    //         count: `Phonebook has info for ${result} people`,
+    //         date: new Date().toString()
+    //     }
+
+    //     JSON.stringify(response)
+
+        // res.toString(result.toString())
+        // res.json(response.toJSON())
+        // res.write(`Phonebook has info for ${result} people\n\n`)
+        // res.write(new Date().toString())
+        // res.send()
+    // })
+
 })
 
 app.get('/api/persons/:id', morgan('tiny'), (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
 
-    //console.log('person: ', person)
-    person ? res.json(person) : res.status(404).end()
+    Person.findById(req.params.id).then(person => {
+        res.json(person.toJSON())
+    })
+    .catch((e) => {
+        console.log('Not found error:', e.message)
+        res.status(404).end()
+    })
 })
 
-app.delete('/api/persons/:id', morgan('tiny'), (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', morgan('tiny'), (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+            .then(result => {
+                res.status(204).end()
+            })
+            .catch(error => next(error))
 })
 
 app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :personToken'), (req, res) => {
     const body = req.body
 
-    if (!body.name) {
+    if (body.name === undefined) {
         return res.status(400).json({
             error: 'name cannot be empty'
         })
-    } else if (!body.number) {
+    } else if (body.number === undefined) {
         return res.status(400).json({
             error: 'number cannot be empty'
         })
-    } 
-    
-    const existence = persons.find(p => p.name === body.name)
-
-    if (existence) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
     }
 
-    const newPerson = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
+    Person.find({}, (error, people) => {
+        if (people.find(p => p.name === body.name)) {
+            return res.status(400).json({
+                error: 'Name must be unique.'
+            })
+        } else {
+            const newPerson = new Person({
+                name: body.name,
+                number: body.number
+            })
 
-    persons = persons.concat(newPerson)
-    res.json(newPerson)
+            newPerson.save().then(savedPerson => {
+                res.json(savedPerson.toJSON())
+            })
+        }
+    })
 })
 
-const generateId = () => {
-    let newId = Math.floor(Math.random() * Math.floor(999999))
 
-    while (persons.find(p => p.id === newId)) {
-        newId = Math.floor(Math.random() * Math.floor(999999))
+/** Error handler middleware*/
+const errorHandler = (error, req, res, next) => {
+    console.log('Error message:', error.message)
+
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return res.status(400).send({error: 'malformatted id'})
     }
 
-    return newId
+    next(error)
 }
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
